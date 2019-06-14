696c90be013241c4760fc86eb66975fd00bfac91
iciue <iciue.hao@gmail.com>
2019-06-14 08:58:38 +0800

    add two number

---
+++ 2-add-two-number.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} l1 (2 -> 4 -> 3) + 
+ * @param {ListNode} l2 (5 -> 6 -> 4)
+ * @return {ListNode}  7 -> 0 -> 8
+ */
+
+var addTwoNumbers = function (l1, l2) {
+  
+};
+
+console.log(
+  addTwoNumbers([2,4,3], [5,6,4])
+);
+
+
