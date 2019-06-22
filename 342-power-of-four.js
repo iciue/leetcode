d83cb4f9f0ca9e7fa38668062c7cc052a3eddcb1
iciue <iciue.hao@gmail.com>
2019-06-22 12:14:41 +0800

    324

---
+++ 342-power-of-four.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+
+ // 将数字转换为 4 进制, 则其二进制永远是 1(4**0) 100(4**1) 1000(4**2)
+ var isPowerOfFour = function (num) {
+  let base4 = num.toString(4)
+  return base4 / Math.pow(10, base4.length - 1) === 1
+};
+
+console.log(isPowerOfFour(127));
