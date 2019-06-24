a464bbb04e821fc49b0aee11b401e10021644f6c
iciue <iciue.hao@gmail.com>
2019-06-24 09:14:45 +0800

    575

---
+++ 575-distribute-candies.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} candies
+ * @return {number}
+ */
+
+// 本质上是数组去重
+var distributeCandies = function (candies) {
+  let kinds = 0
+  let candiesLen = candies.length
+  candies = candies.sort()
+  for (let i = 0; i < candiesLen; i++) {
+    let curr = candies[i]
+    let next = candies[i+1]
+    if(curr !== next) kinds++
+  }
+
+  return kinds > candiesLen/2 ? candiesLen/2 : kinds
+};
+
+
+console.log(distributeCandies([100, 0, 100, 0, 100, 0, 100, 0, 100, 0, 100, 0]))
+// console.log(distributeCandies([1, 1, 1, 1, 2, 2, 2, 3, 3, 3]));
