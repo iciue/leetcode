2c735fcf6e975b6b7fe652f84fa64b6c79132e9f
iciue <iciue.hao@gmail.com>
2019-07-05 22:47:12 +0800

    21

--- 21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists.js
@@ -12,7 +12,25 @@
  */
 
 var mergeTwoLists = function (l1, l2) {
-  
+  let res = new ListNode()
+
+  function insert(node1, node2) {
+    if (node1 === null && node2 === null) return null
+    if (!node1) return node2.val
+    if (!node2) return node1.val
+    let currNode = new ListNode()
+    if (node1.val < node2.val) {
+      currNode.val = node1.val
+      currNode.next = insert(node1.next, node2)
+    } else {
+      currNode.val = node2.val
+      currNode.next = insert(node1, node2.next)
+    }
+    return currNode
+  }
+  res = insert(l1, l2)
+
+  return res
 };
 
 
