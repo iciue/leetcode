6ffdd01a323137d8d68e2c4d6e578515197d5b20
iciue <iciue.hao@gmail.com>
2019-07-26 09:16:55 +0800

    867

---
+++ 867.transpose-matrix.js
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=867 lang=javascript
+ *
+ * [867] Transpose Matrix
+ *
+ * https://leetcode.com/problems/transpose-matrix/description/
+ *
+ * algorithms
+ * Easy (63.89%)
+ * Likes:    214
+ * Dislikes: 229
+ * Total Accepted:    49.5K
+ * Total Submissions: 77.4K
+ * Testcase Example:  '[[1,2,3],[4,5,6],[7,8,9]]'
+ *
+ * Given a matrix A, return the transpose of A.
+ * 
+ * The transpose of a matrix is the matrix flipped over it's main diagonal,
+ * switching the row and column indices of the matrix.
+ * 
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [[1,4,7],[2,5,8],[3,6,9]]
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: [[1,2,3],[4,5,6]]
+ * Output: [[1,4],[2,5],[3,6]]
+ * 
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * 1 <= A.length <= 1000
+ * 1 <= A[0].length <= 1000
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @param {number[][]} A
+ * @return {number[][]}
+ */
+
+var transpose = function(A) {
+  return A[0].map( (_, i) => {
+    return A.map( row => row[i])
+  })
+}
+
+var transpose = function(A) {
+  return A[0].map( (_, i) => {
+    return A.reduce( (row, _, j) => {
+      row.push(A[j][i])
+      return row
+    }, [])
+  })
+};
+
