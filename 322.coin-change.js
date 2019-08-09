1fc5c83cf0a4a77a1838d475e79f4b0a4aae95aa
iciue <iciue.hao@gmail.com>
2019-08-09 09:26:11 +0800

    322

--- 322.coin-change.js
+++ 322.coin-change.js
@@ -41,7 +41,28 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function (coins, amount, memo = {}) {
+
+// DP
+var coinChange = function (coins, amount) {
+  const dp = Array(amount + 1).fill(Infinity)
+  dp[0] = 0
+
+  for (let i = 1; i <= amount; i++) {
+    for (let j = 0; j < coins.length; j++) {
+      let min = Infinity
+      if (i >= coins[j]) {
+        min = Math.min(dp[i - coins[j]] + 1, dp[i])
+      }
+      if (min < dp[i]) dp[i] = min
+    }
+  }
+
+  return dp[amount] === Infinity ? -1 : dp[amount]
+}
+
+
+// 递归 + 缓存
+var coinChange1 = function (coins, amount, memo = {}) {
   if (amount === 0) return 0
   if (memo[amount] !== undefined) return memo[amount]
 
@@ -65,7 +86,7 @@ var coinChange = function (coins, amount, memo = {}) {
 };
 
 console.log(
-  coinChange([2], 3),
+  // coinChange([2], 3),
   coinChange([1, 2, 5], 6),
-  coinChange([186, 419, 83, 408], 6249),
+  // coinChange([186, 419, 83, 408], 6249),
 );
