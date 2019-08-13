ebad2b4bd0a07e6d4fc9a2a2f1442fe5872130fb
iciue <iciue.hao@gmail.com>
2019-08-13 13:55:46 +0800

    8

---
+++ 8.string-to-integer-atoi.js
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode id=8 lang=javascript
+ *
+ * [8] String to Integer (atoi)
+ *
+ * https://leetcode.com/problems/string-to-integer-atoi/description/
+ *
+ * algorithms
+ * Medium (14.74%)
+ * Likes:    1040
+ * Dislikes: 6504
+ * Total Accepted:    398.3K
+ * Total Submissions: 2.7M
+ * Testcase Example:  '"42"'
+ *
+ * Implement atoi which converts a string to an integer.
+ * 
+ * The function first discards as many whitespace characters as necessary until
+ * the first non-whitespace character is found. Then, starting from this
+ * character, takes an optional initial plus or minus sign followed by as many
+ * numerical digits as possible, and interprets them as a numerical value.
+ * 
+ * The string can contain additional characters after those that form the
+ * integral number, which are ignored and have no effect on the behavior of
+ * this function.
+ * 
+ * If the first sequence of non-whitespace characters in str is not a valid
+ * integral number, or if no such sequence exists because either str is empty
+ * or it contains only whitespace characters, no conversion is performed.
+ * 
+ * If no valid conversion could be performed, a zero value is returned.
+ * 
+ * Note:
+ * 
+ * 
+ * Only the space character ' ' is considered as whitespace character.
+ * Assume we are dealing with an environment which could only store integers
+ * within the 32-bit signed integer range: [−2^31,  2^31 − 1]. If the numerical
+ * value is out of the range of representable values, INT_MAX (2^31 − 1) or
+ * INT_MIN (−2^31) is returned.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "42"
+ * Output: 42
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "   -42"
+ * Output: -42
+ * Explanation: The first non-whitespace character is '-', which is the minus
+ * sign.
+ * Then take as many numerical digits as possible, which gets 42.
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "4193 with words"
+ * Output: 4193
+ * Explanation: Conversion stops at digit '3' as the next character is not a
+ * numerical digit.
+ * 
+ * 
+ * Example 4:
+ * 
+ * 
+ * Input: "words and 987"
+ * Output: 0
+ * Explanation: The first non-whitespace character is 'w', which is not a
+ * numerical 
+ * digit or a +/- sign. Therefore no valid conversion could be performed.
+ * 
+ * Example 5:
+ * 
+ * 
+ * Input: "-91283472332"
+ * Output: -2147483648
+ * Explanation: The number "-91283472332" is out of the range of a 32-bit
+ * signed integer.
+ * Thefore INT_MIN (−2^31) is returned.
+ * 
+ */
+// 不包含空白字符 返回 有符号整数
+// 首个非空字符后 非数字, 非+-符号, 返回 0
+// 首个非空字符后 数字, +-符号, 返回有符号整数
+// 超出 32-bit 整型, 则返回 2147483648
+
+const MAX_SAFE_INTEGER = 2147483647
+const MIN_SAFE_INTEGER = -2147483648
+
+var myAtoi = function (str) {
+  const reg = /^\s{0,}((?:\+\d|\-\d|\d*)\d*)/
+  const match = str.match(reg)
+  if (!match) return 0
+  
+  const number = match[1] * 1
+  if (number > MAX_SAFE_INTEGER) return MAX_SAFE_INTEGER
+  if (number < MIN_SAFE_INTEGER) return MIN_SAFE_INTEGER
+  return number
+};
